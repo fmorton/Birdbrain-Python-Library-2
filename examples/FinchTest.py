03f22b19f8442769f8ac90f2ec4e9394a92c4817
Frank Morton <fmorton@mac.com>
2025-01-20 19:43:43 -0500

    add basic test structure

--- examples/FinchTest.py
+++ examples/FinchTest.py
@@ -6,8 +6,8 @@ myFinch = Finch('A')
 
 for i in range(0, 10):
     myFinch.setBeak(100, 100, 100)
-    time.sleep(1)
+    time.sleep(0.1)
     myFinch.setBeak(0, 0, 0)
-    time.sleep(1)
+    time.sleep(0.1)
 
 myFinch.stopAll()
--- examples/HummingbirdTest.py
+++ examples/HummingbirdTest.py
@@ -6,7 +6,8 @@ myBird = Hummingbird('A')
 
 for i in range(0, 10):
     myBird.setLED(1, 100)
-    time.sleep(1)
+    time.sleep(0.1)
     myBird.setLED(1, 0)
-    time.sleep(1)
+    time.sleep(0.1)
+
 myBird.stopAll()
