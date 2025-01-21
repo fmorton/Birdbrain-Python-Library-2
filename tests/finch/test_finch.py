091315cd0dfc64d6b9d92f74b0c205122d4fe529
Frank Morton <fmorton@mac.com>
2025-01-20 19:41:21 -0500

    add basic test structure

--- tests/finch/test_finch.py
+++ tests/finch/test_finch.py
@@ -2,12 +2,17 @@ import time
 
 from BirdBrain import Finch
 
-myFinch = Finch('A')
+def test_no_device():
+    finch = Finch('C')
 
-for i in range(0, 10):
-    myFinch.setBeak(100, 100, 100)
-    time.sleep(1)
-    myFinch.setBeak(0, 0, 0)
-    time.sleep(1)
 
-myFinch.stopAll()
+def test_break():
+    finch = Finch('A')
+
+    for i in range(0, 10):
+        finch.setBeak(100, 100, 100)
+        time.sleep(0.1)
+        finch.setBeak(0, 0, 0)
+        time.sleep(0.1)
+
+        finch.stopAll()
