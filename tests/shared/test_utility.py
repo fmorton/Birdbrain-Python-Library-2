2b1cc8617448bb18efec4840cfe81409084f6aa8
Frank Morton <fmorton@mac.com>
2025-11-05 13:10:50 -0500

    improve test coverage for version 0.10.1

--- tests/shared/test_utility.py
+++ tests/shared/test_utility.py
@@ -19,6 +19,17 @@ def test_bounds():
     assert Utility.bounds(999999, -100, 100) == 100
     assert Utility.bounds(-999999, -100, 100) == -100
 
+    assert Utility.bounds(10.0, 0.0, 100.0) == 10.0
+    assert Utility.bounds(10.0, -100.0, 100.0) == 10.0
+    assert Utility.bounds(-10.0, -100.0, 100.0) == -10.0
+    assert Utility.bounds(-100.0, -100.0, 100.0) == -100.0
+    assert Utility.bounds(100.0, -100.0, 100.0) == 100.0
+
+    assert Utility.bounds(101.0, -100.0, 100.0) == 100.0
+    assert Utility.bounds(-101.0, -100.0, 100.0) == -100.0
+    assert Utility.bounds(999999.0, -100.0, 100.0) == 100.0
+    assert Utility.bounds(-999999.0, -100.0, 100.0) == -100.0
+
     assert Utility.bounds(str(10), str(0), str(100)) == 10
     assert Utility.bounds(str(10), str(-100), str(100)) == 10
     assert Utility.bounds(str(-10), str(-100), str(100)) == -10
@@ -30,6 +41,39 @@ def test_bounds():
     assert Utility.bounds(str(999999), str(-100), str(100)) == 100
     assert Utility.bounds(str(-999999), str(-100), str(100)) == -100
 
+def test_decimal_bounds():
+    assert Utility.decimal_bounds(10, 0, 100) == 10
+    assert Utility.decimal_bounds(10, -100, 100) == 10
+    assert Utility.decimal_bounds(-10, -100, 100) == -10
+    assert Utility.decimal_bounds(-100, -100, 100) == -100
+    assert Utility.decimal_bounds(100, -100, 100) == 100
+
+    assert Utility.decimal_bounds(101, -100, 100) == 100
+    assert Utility.decimal_bounds(-101, -100, 100) == -100
+    assert Utility.decimal_bounds(999999, -100, 100) == 100
+    assert Utility.decimal_bounds(-999999, -100, 100) == -100
+
+    assert Utility.decimal_bounds(10.0, 0.0, 100.0) == 10.0
+    assert Utility.decimal_bounds(10.0, -100.0, 100.0) == 10.0
+    assert Utility.decimal_bounds(-10.0, -100.0, 100.0) == -10.0
+    assert Utility.decimal_bounds(-100.0, -100.0, 100.0) == -100.0
+    assert Utility.decimal_bounds(100.0, -100.0, 100.0) == 100.0
+
+    assert Utility.decimal_bounds(101.0, -100.0, 100.0) == 100.0
+    assert Utility.decimal_bounds(-101.0, -100.0, 100.0) == -100.0
+    assert Utility.decimal_bounds(999999.0, -100.0, 100.0) == 100.0
+    assert Utility.decimal_bounds(-999999.0, -100.0, 100.0) == -100.0
+
+    assert Utility.decimal_bounds(str(10), str(0), str(100)) == 10
+    assert Utility.decimal_bounds(str(10), str(-100), str(100)) == 10
+    assert Utility.decimal_bounds(str(-10), str(-100), str(100)) == -10
+    assert Utility.decimal_bounds(str(-100), str(-100), str(100)) == -100
+    assert Utility.decimal_bounds(str(100), str(-100), str(100)) == 100
+
+    assert Utility.decimal_bounds(str(101), str(-100), str(100)) == 100
+    assert Utility.decimal_bounds(str(-101), str(-100), str(100)) == -100
+    assert Utility.decimal_bounds(str(999999), str(-100), str(100)) == 100
+    assert Utility.decimal_bounds(str(-999999), str(-100), str(100)) == -100
 
 def test_flatten():
     flattened = Utility.flatten_string(["something", "1", ["A", "B"], "2", "else", 99, [99]])
