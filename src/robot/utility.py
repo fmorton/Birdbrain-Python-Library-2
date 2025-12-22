1427f92cf9275992d4ded5bd0c3139ac4295f5ab
Frank Morton <fmorton@mac.com>
2025-12-22 14:45:41 -0500

    cleanup for pylint

--- src/robot/utility.py
+++ src/robot/utility.py
@@ -10,37 +10,34 @@ class Utility:
     def is_none_or_empty(s):
         if s is None or s == "":
             return True
-        else:
-            return False
+
+        return False
 
     @staticmethod
-    def bounds(input, input_min, input_max, pass_through_input=None):
-        # if pass_through_input is not None and (input == pass_through_input): return int(input)
-
-        if int(input) < int(input_min):
+    def bounds(utility_input, input_min, input_max):
+        if int(utility_input) < int(input_min):
             return int(input_min)
-        if int(input) > int(input_max):
+        if int(utility_input) > int(input_max):
             return int(input_max)
 
-        return int(input)
+        return int(utility_input)
 
     @staticmethod
-    def decimal_bounds(input, input_min, input_max, pass_through_input=None):
-        # if pass_through_input is not None and (input == pass_through_input): return int(input)
-
-        if float(input) < float(input_min):
+    def decimal_bounds(utility_input, input_min, input_max):
+        if float(utility_input) < float(input_min):
             return float(input_min)
-        if float(input) > float(input_max):
+        if float(utility_input) > float(input_max):
             return float(input_max)
 
-        return float(input)
+        return float(utility_input)
 
     @staticmethod
     def flatten_string(original_list, divider="/"):
         if isinstance(original_list[0], list):
             original_list = original_list[0]
 
-        original_list = [item for item in original_list]
+        ###DEBUG: original_list = [item for item in original_list]
+        original_list = list(original_list)
 
         s = ""
         for element in list(original_list):
